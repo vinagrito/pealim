ecc568391dfc0a1e645627b4f027cfa6bbb2726b
Daniel Garcia Shulman <vinagrito@gmail.com>
2013-03-31 14:31:22 +0300

    Old working routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Pealim::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-
-  match 'application/set_user_lang' => 'application#set_user_lang'
+  match "application/set_user_lang" => "application#set_user_lang"
 
   resources :verbs
-
   resources :comments, :only => [:index, :show, :new]
-
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -63,5 +60,5 @@ Pealim::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id(.:format)))'
+  match ':controller(/:action(/:id(.:format)))'
 end
